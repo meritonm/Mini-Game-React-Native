df6db4976484b677d932d306888fd6ca4837781e
Meriton <musliumeriton@gmail.com>
2024-11-12 16:46:25 +0100

    adding background image and style with opacity

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screen-components/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
     <LinearGradient colors={["#9d0651", "#dfb322"]} style={styles.appContainer}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.appContainer}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +21,8 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     // backgroundColor: "#b6ae91",
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
