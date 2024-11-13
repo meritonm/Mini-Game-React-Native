cbf5ac847930aad39752aa93cb65fd9688bc6e89
Meriton <musliumeriton@gmail.com>
2024-11-13 15:25:22 +0100

    add style and safe area for phone with notches

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import StartGameScreen from "./screen-components/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screen-components/GameScreen";
@@ -25,7 +25,7 @@ export default function App() {
         style={styles.appContainer}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.appContainer}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
